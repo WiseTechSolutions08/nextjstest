178e995d602b009e98cd602b0a0eaee8eb71c19f
vinoth.kr <vinoth.kr@zohocorp.com>
2022-01-21 01:29:45 +0530

    .

--- autodep/settings.py
+++ autodep/settings.py
@@ -12,7 +12,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'catalog',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
