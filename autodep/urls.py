4b7e4843f621c8ac388931569133cf3080f26534
vinoth.kr <vinoth.kr@zohocorp.com>
2022-01-21 01:07:15 +0530

    Urls fix

--- autodep/urls.py
+++ autodep/urls.py
@@ -5,9 +5,9 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('api/', include('catalog.api.urls'))
+    path('api/', include('catalog.api.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
 
 
