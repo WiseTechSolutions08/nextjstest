ad8fc1c9145ef44fe4dc31aa3d5f24e638e69726
vinoth.kr <vinoth.kr@zohocorp.com>
2022-01-20 19:21:43 +0530

    ALlowed hosts fix

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
-export const localhost = "http://127.0.0.1:8000/";
-// export const localhost = "https://herokugit-auto-deploy.herokuapp.com/";
+// export const localhost = "http://127.0.0.1:8000/";
+export const localhost = "https://testapp-8098.herokuapp.com/";
 export const api = "api/";
 export const endpoint = `${localhost}${api}`;
 export const productsListUrl = `${endpoint}products-list`;
