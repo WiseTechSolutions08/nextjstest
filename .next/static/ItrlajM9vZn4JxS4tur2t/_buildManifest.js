117a94176848dc6c950c44a9bcbfa5c7edb4e0da
vinoth.kr <vinoth.kr@zohocorp.com>
2022-01-20 19:17:19 +0530

    Inital commit

---
+++ .next/static/ItrlajM9vZn4JxS4tur2t/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e9a675efd5eefb396509.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-61034ae38db8f7d5cc59.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vinoth-5221/Documents/DA-WT/nextjstest/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
